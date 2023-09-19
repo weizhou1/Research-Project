295c4794b2f054b7fced8fa14ced7524dd08864f
weizhou1 <771560745@qq.com>
2023-09-19 20:48:04 +1000

    update main.py

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight for
 parser.add_argument('--patience', type=int, default=20, help='Patience for early stopping (# of epochs).')
 parser.add_argument('--lamb', type=float, default=0.2, help='Weight to balance constraints.')
 
-# args = parser.parse_args()
 
 # Composition argument 
 parser.add_argument('-f','--fasta_file', default = "Sim-5G",help='specify the fasta file.')
@@ -52,7 +51,6 @@ print('----------args----------\n')
 def main():
 
 	assembly_graph, constraints, ground_truth, Gx ,filtered_nodes = load_data(args.dataset)
-	# assembly_graph, constraints, ground_truth, Gx  = load_data(args.dataset)
 	triplets = sample_constraints(constraints, ground_truth)
 
 	diff = Graph_Diffusion_Convolution(assembly_graph, args.alpha, args.eps)
@@ -62,30 +60,11 @@ def main():
 	adj = sparse_to_tuple(adj)
 
 	feats = assembly_graph.todense() 
-	# print("assembly_graph",feats.shape)
-	# print("edge num :",np.matrix.sum(feats))
 
 		
-	# composition, coverage = load_feat(args)
 	composition = np.array(load_feat(args))
-	# composition = np.array(composition) 
-	# coverage = np.array(coverage) 
 	composition = np.delete(composition,filtered_nodes, axis= 0)
-	# print(composition)
-	# coverage = np.delete(coverage,filtered_nodes, axis= 0)
-
-	# normed_coverage= normalize(coverage, axis = 0 ,norm='l1')
-	# normed_composition= normalize(composition, axis = 1, norm='l1')
-
-	# print("coverage",coverage.shape)
-	# print(coverage)
-	
-	# normed_comp_cov = composition * coverage[:, None]                     
-	# normed_comp_cov = normalize(normed_comp_cov, axis=1, norm='l1')
-	# print("normalise coposiiton and coverage ",normed_comp_cov.shape)
 	composition = normalize(composition, axis=1, norm='l1')
-	# print("normalise coposiiton ",composition.shape)
-	# print(composition)
 	feats = np.concatenate((composition, feats), axis=1) 
 	
 
@@ -104,8 +83,6 @@ def main():
 	print("bin size = ", n_clusters)
 
 
-
-	# model = Learning(feats.shape[1], args.hid_dim, args.n_clusters, args)
 	model = Learning(feats.shape[1], args.hid_dim,n_clusters, args)
 	pred_labels = model.train(adj, diff,feats, Gx, triplets, constraints, ground_truth, assembly_graph,composition)
 	
@@ -114,44 +91,11 @@ def main():
 	print ("### Precision = %0.4f, Recall = %0.4f, F1 = %0.4f, ARI = %0.4f, NMI = %0.4f"  % (p,r,f1,ari,nmi))
 
 
-	np.save('plot/Yujia/data/Preds_MixBin_'+ args.dataset+'.npy',pred_labels)
+	# np.save('plot/Yujia/data/Preds_MixBin_'+ args.dataset+'.npy',pred_labels)
 
 	return p,r,f1,ari,nmi 
 
 
 if __name__ == '__main__':
-	# # get the start time
-	# st = time.process_time()
 
 	main()
-
-	# # get the end time
-	# et = time.process_time()
-
-	# # get execution time
-	# res = et - st
-	# print('CPU Execution time:', res/60, 'minutes')
-	
-
-	# N = 5
-	# perfom = [[] for _ in range(N)]
-	# cpu_time = [[] for _ in range(N)]
-	# for i in range(5):
-	# 	# get start time
-	# 	st = time.process_time()
-	# 	p,r,f1,ari,nmi = main()
-	# 	# get the end time
-	# 	et = time.process_time()
-	# 	perfom[i] = [p,r,f1,ari,nmi]
-
-	# 	# get execution time
-	# 	res = et - st
-	# 	print('CPU Execution time:', res/60, 'minutes')
-	# 	cpu_time[i] = res
-
-	# print("performance")
-	# for i in range(N):
-	# 	print(perfom[i])
-	# 	print('CPU Execution time:',cpu_time[i],"seconds")
-	# 	print("\n")
-
